45e229a5c2362c962b31483614bf7646c4912d91
Tosin Akinyele <tosinakinyele@Tosins-MacBook-Pro.local>
2020-01-21 20:15:12 +0100

    implement model

--- config.py
+++ config.py
@@ -10,4 +10,5 @@ DEBUG = True
 
 
 # TODO IMPLEMENT DATABASE URL
-SQLALCHEMY_DATABASE_URI = '<Put your local database url>'
+SQLALCHEMY_DATABASE_URI = 'postgres://tosinakinyele@localhost:5432/fyyurdb'
+DATABASE_URL = SQLALCHEMY_DATABASE_URI
